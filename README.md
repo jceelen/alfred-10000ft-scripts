f2bf65926d55d54167205a2363af7690a1f96f76
Jasper Ceelen <ceelen@gmail.com>
2016-06-18 19:16:30 +0200

    - Added submenu with options for every project - Changed usage - Add create report for project - Add view project - Fixed caching - Simplified workflow in Alfred - Added project icons based on status (confirmed, tentative, internal)

--- README.md
+++ README.md
@@ -11,17 +11,35 @@ This script is a very simple first version and a research project and not really
 ## Download and installation ##
 Download the latest version from the [GitHub releases page](https://github.com/jceelen/alfred-10000ft-scripts/releases/latest)
 
+Copy your API key to the clipboard, and use `.10ksetkey` to save your API key
+
 [Request your 10.000ft API Key](http://10000ft.us6.list-manage2.com/subscribe?u=9c2ec42cba0e4196798807690&id=be8afd4d34)
 
 ## Usage ##
-First run: Copy your API key to the clipboard, and use `.10ksetkey` to save your API key
-
-- `10kp` — shows a list of active projects which you can filter
+- `.10ksetkey` — saves your 10.000ft API key to the OSX Keychain
+- `.10ksetuser` — sets your 10.000ft User-tag-name (for feature in the future)
+- `.10kupdate` — forces update of your 10.000ft project data
+- `10k projects` — shows a list of active projects which you can filter
 
 ### Shortcuts ###
 - `10k home` — open your 10000ft.com homepage
 - `10k schedule` — open the Schedule view
 - `10k analytics` — open the Analytics view
+
+## Changelog
+
+#### v0.4
+- Added submenu with options for every project
+- Changed usage
+- Add create report for project
+- Add view project
+- Fixed caching
+- Simplified workflow in Alfred
+- Added project icons based on status (confirmed, tentative, internal)
+
+#### v0.3
+- Added automatic updates
+
 
 ## Thanks ##
 This workflow uses the awesome [Alfred-Workflow](http://www.deanishe.net/alfred-workflow/) library [MIT-licensed](http://opensource.org/licenses/MIT).
--- src/10000ft.py
+++ src/10000ft.py
@@ -1,9 +1,11 @@
 # encoding: utf-8
 import sys
 import argparse
+from urllib import urlencode, unquote_plus
 from workflow import (Workflow, ICON_WEB, ICON_INFO, ICON_WARNING, PasswordNotFound)
 from workflow.background import run_in_background, is_running
 
+# Update data
 UPDATE_SETTINGS = {'github_slug': 'jceelen/alfred-10000ft-scripts'}
 ICON_UPDATE = 'update-available.png'
 
@@ -12,26 +14,34 @@ HELP_URL = 'https://github.com/jceelen/alfred-10000ft-scripts/issues'
 
 log = None
 
-
 def search_key_for_project(project):
     """Generate a string search key for a post"""
     elements = []
     elements.append(project['name'])  # projectnaam
     elements.append(project['client'])  # klant
+    elements.append(project['project_state'])  # status
     elements.append(str(project['project_code']))  # projectcode
     return u' '.join(elements)
 
 
+def get_project_data(project_id):
+    """Find the project matching the project_id"""
+    projects = wf.cached_data('projects', None, max_age=0)
+        
+    #loop through projects and return project with a match
+    for project in projects:     
+        if int(project['id']) == int(project_id):
+            return project
 
-def main(wf):
+def main(wf):   
+    wf.logger.info('main started')
     # Update available?
     if wf.update_available:
         wf.add_item('A newer version is available',
-                    '↩ to install update',
+                    'Press ENTER to install update',
                     autocomplete='workflow:update',
                     icon=ICON_UPDATE)
 
-    log.debug('Main Started')
     # build argument parser to parse script args and collect their
     # values
     parser = argparse.ArgumentParser()
@@ -39,6 +49,8 @@ def main(wf):
     # value to 'apikey' (dest). This will be called from a separate "Run Script"
     # action with the API key
     parser.add_argument('--setkey', dest='apikey', nargs='?', default=None)
+    parser.add_argument('--setuser', dest='user', nargs='?', default=None)
+    parser.add_argument('--options', dest='project_id', nargs='?', default=None)
     # add an optional query and save it to 'query'
     parser.add_argument('query', nargs='?', default=None)
     # parse the script's arguments
@@ -52,6 +64,10 @@ def main(wf):
     if args.apikey:  # Script was passed an API key
         # save the key
         wf.save_password('10k_api_key', args.apikey)
+        return 0  # 0 means script exited cleanly
+    if args.user:  # Script was passed an API key
+        # save the key
+        wf.settings['user'] = args.user.lower()
         return 0  # 0 means script exited cleanly
 
     ####################################################################
@@ -81,7 +97,7 @@ def main(wf):
     projects = wf.cached_data('projects', None, max_age=0)
 
     # Start update script if cached data is too old (or doesn't exist)
-    if not wf.cached_data_fresh('posts', max_age=60):
+    if not wf.cached_data_fresh('projects', max_age=600):
         cmd = ['/usr/bin/python', wf.workflowfile('update.py')]
         run_in_background('update', cmd)
 
@@ -95,31 +111,87 @@ def main(wf):
     if query and projects:
         projects = wf.filter(query, projects, key=search_key_for_project, min_score=20)
 
-    if not projects:  # we have no data to show, so show a warning and stop
+    # we have no data to show, so show a warning and stop
+    if not projects:  
         wf.add_item('No projects found', icon=ICON_WARNING)
         wf.send_feedback()
         return 0
 
-    # Loop through the returned projects and add an item for each to
-    # the list of results for Alfred
-    for project in projects:
-        wf.add_item(title=project['name'],
-                    subtitle='Client: ' + project['client'] + '. Project state: ' + project['project_state'],
-                    modifier_subtitles={
-                        #'shift': 'Subtext when shift is pressed',
-                        #'fn': 'Subtext when fn is pressed',
-                        #'ctrl': 'Subtext when ctrl is pressed',
-                        'alt': 'tags: ' + str(project['tags']['data']),
-                        'cmd': 'id: ' + str(project['id']) + 'guid: ' + project['guid']
-                    },
-                    arg=str(project['id']),
+    ####################################################################
+    # Show options for project
+    ####################################################################
+
+    # If project_id was passed on in the query, show the options for manipulating a project.
+    if args.project_id:
+        from datetime import datetime
+        now = datetime.now()
+
+        # Get current project data
+        project = get_project_data(args.project_id)
+
+        # Build report URL
+        params = {'view' : 10, #8 = Fees, 10 = hours
+                  'time' : 4,
+                  'start' : project['starts_at'],
+                  'end' : project['ends_at'],
+                  'firstgroup' : 'phase_name',
+                  'secondgroup' : 'user_name',
+                  'filters' : '[["' +project['name']+ '"],[],["' +project['client']+ '"],[],[],[],[],["Confirmed","Future"],[],[],[],[],[],[]]',
+                  'version' : 2,
+                  'title' : project['name'] + ' - %s-%s-%s' % (now.day, now.month, now.year)
+                    }
+
+        params = unquote_plus(urlencode(params))
+        wf.logger.debug('reports?' + params)
+
+        # Add options for projects 
+        wf.add_item(title='View project',
+                    subtitle=project['name'],
+                    arg='viewproject?id=' + str(project['id']),
                     valid=True,
-                    icon=ICON_WEB,
-                    copytext=project['name'])
-    
-    # Send the results to Alfred as XML
-    wf.send_feedback()
-    return 0
+                    icon='icons/icon_project_{0}.png'.format(project['project_state']).lower())
+        wf.add_item(title='Edit project',
+                    subtitle=project['name'],
+                    arg='editproject?id=' + str(project['id']),
+                    valid=True,
+                    icon='icons/icon_project_{0}.png'.format(project['project_state']).lower())
+        wf.add_item(title='Run report for project',
+                    subtitle=project['name'],
+                    arg='reports?' + params,
+                    valid=True,
+                    icon='icons/icon_project_{0}.png'.format(project['project_state']).lower())
+        wf.send_feedback()
+
+    # Show list of projects
+    else: 
+        # Loop through the returned projects and add an item for each to
+        # the list of results for Alfred
+        for project in projects:
+            
+            # WIP Extract tags from data for every project and put them in a list
+            tags = project['tags']['data']
+            taglist = []
+            for tag in tags:
+                taglist.append(tag['value'])
+            
+            wf.add_item(title=project['name'],
+                        subtitle='ENTER to view project, press ALT to show more info.',
+                        modifier_subtitles={
+                            #'shift': 'Subtext when shift is pressed',
+                            #'fn': 'Subtext when ctrl is pressed',
+                            'alt': 'Client: ' + project['client'] + ' | Tags: ' + str(taglist),
+                            'ctrl': 'View in 10.000ft',
+                            'cmd': 'Edit in 10.000ft, CMD+C to copy name.'
+                            },
+                        arg=str(project['id']),
+                        valid=True,
+                        icon='icons/icon_project_{0}.png'.format(project['project_state']).lower(),
+                        copytext=project['name'])
+        
+        # Send the results to Alfred as XML
+        wf.send_feedback()
+        return 0
+
 
 if __name__ == '__main__':
     wf = Workflow(help_url=HELP_URL,
